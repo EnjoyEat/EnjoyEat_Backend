a043e06d5041f67818373a55190bb6b6992e7329
Glory <dudrhkd6550@naver.com>
2022-10-14 15:01:56 +0900

    first commit

---
+++ src/main/java/com/example/EnjoyEat/EnjoyEatApplication.java
@@ -0,0 +1,11 @@
+package com.example.EnjoyEat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyEatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyEatApplication.class, args);
+	}
+}
