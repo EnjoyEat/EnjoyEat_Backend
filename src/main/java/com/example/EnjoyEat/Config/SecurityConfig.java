9d72a0cd684a387005d07ae3b8ee2951ca59118e
yewon <ajdanddl6321@gmail.com>
2022-10-24 14:59:22 +0900

    feat: UserOAuth2Service 추가

--- src/main/java/com/example/EnjoyEat/Config/SecurityConfig.java
+++ src/main/java/com/example/EnjoyEat/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
-package com.example.EnjoyEat.Model.config;
+package com.example.EnjoyEat.Config;
 
+import com.example.EnjoyEat.OAuth.UserOAuth2Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +10,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity// 해당 파일로 시큐리티를 활성화 -> 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
 @Configuration // IoC
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserOAuth2Service userOAuth2Service;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -18,6 +24,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginProcessingUrl("auth/signin")
                 .defaultSuccessUrl("/")
                 .and()
-                .oauth2Login();
+                .oauth2Login()
+                .defaultSuccessUrl("/") // oauth2 인증이 성공했을 때, 이동되는 url을 설정.
+                // .successHandler(oAuth2AuthenticationSuccessHandler) // 인증 프로세스에 따라 사용자 정의 로직을 실행.
+                .userInfoEndpoint()
+                .userService(userOAuth2Service); // 로그인이 성공하면 해당 유저의 정보를 들고 customOAuth2UserService에서 후처리.
     }
 }
--- src/main/java/com/example/EnjoyEat/Repository/UserRepository.java
+++ src/main/java/com/example/EnjoyEat/Repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.EnjoyEat.Repository;
 import com.example.EnjoyEat.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
 }
